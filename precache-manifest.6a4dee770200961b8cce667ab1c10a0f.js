6c8ec0ced18c425b0c5430b046c621d6c25012be
Sahithi Yarlagadda <sahithiyarlagadda79@gmail.com>
2019-05-03 14:42:07 +0530

    Updates

---
+++ precache-manifest.6a4dee770200961b8cce667ab1c10a0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41eeb173611dce8f3e50eccb1d31ea8",
+    "url": "/Sahithi/index.html"
+  },
+  {
+    "revision": "34868b50c1a56997a527",
+    "url": "/Sahithi/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Sahithi/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "34868b50c1a56997a527",
+    "url": "/Sahithi/static/js/main.a59cbae3.chunk.js"
+  },
+  {
+    "revision": "994a5c5b8c94a1b002f4",
+    "url": "/Sahithi/static/js/runtime~main.06423617.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Sahithi/static/media/logo.5d5d9eef.svg"
+  }
+]);
